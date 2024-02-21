fe9bc01e24b4e8869a42e852473bd80a6704ae1c
BuschMath <busch@iowacentral.edu>
2024-02-21 13:21:16 -0600

    Blue

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 public class Main {
     static public void main(String[] args){
 
-        Icon icon = new TestIcon(20);
+        Icon icon = new TestIcon(50);
 
         JOptionPane.showMessageDialog(null, "Test Message", 
         "Dialog Box", JOptionPane.INFORMATION_MESSAGE, icon);
--- TestIcon.java
+++ TestIcon.java
@@ -18,7 +18,7 @@ public class TestIcon implements Icon {
     public void paintIcon(Component c, Graphics g, int x, int y){
         Graphics2D g2 = (Graphics2D) g;
         Ellipse2D.Double planet = new Ellipse2D.Double(x, y, size, size);
-        g2.setColor(Color.RED);
+        g2.setColor(Color.BLUE);
         g2.fill(planet);
     }
 
